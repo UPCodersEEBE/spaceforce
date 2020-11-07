2cae04855d44cd8a35da0ad4f3e7e7f263f53bf0
a96lex <a96lex@gmail.com>
2020-11-07 16:45:11 +0100

    some adjustements

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import math
 from moon import Moon
 
 pygame.init()
+font = pygame.font.SysFont("8-Bit-Madness", 15, True, False)
 
-pygame.font.init()
-myfont = pygame.font.SysFont("Comic Sans MS", 30)
 
 WHITE1 = (255, 255, 255)
 WHITE2 = (190, 190, 190)
 WHITE3 = (45, 45, 45)
+EXPLOSION = (200, 20, 20)
 BLACK = (0, 0, 0)
 LIGHTBLUE = (0, 176, 240)
+ball_color = LIGHTBLUE
 
-x, y = 000, 500
-vx, vy = 1, 0
+x, y = 000, 50
+vx, vy = 0, 5
+rad = 3
+av = 2
 
-screen_width, screen_height = 800, 600
-scaling_factor = 1
+screen_width, screen_height = 240, 160
+scaling_factor = 4
 
 pygame.init()
 win = pygame.display.set_mode(
@@ -26,47 +29,53 @@ win = pygame.display.set_mode(
 screen = pygame.Surface((screen_width, screen_height))
 
 # (x,y,radi)
-all_moons = [(240, 400, 20), (240, 601, 30)]
+all_moons = [(240, 40, 20), (100, 130, 30)]
 
 
 def get_forces():
     fx = 0
-    fy = -1
+    fy = 0
     stop = False
     for moon in all_moons:
         ax = moon[0] - x
         ay = moon[1] - y
-        m = 3.14159256536 * 4 * moon[2] ** 3 / 30
+        m = 1 * moon[2] ** 3 / 30
         f = m * 10 / (ax ** 2 + ay ** 2)
         o = math.atan(ay / ax)
         fx += f * abs(math.cos(o)) * ax / abs(ax)
         fy += f * abs(math.sin(o)) * ay / abs(ay)
-        if abs(ay) < moon[2] and abs(ax) < moon[2]:
+        if abs(ay) - rad < moon[2] and abs(ax) - rad < moon[2]:
             stop = True
     return fx, fy, stop
 
 
 run = True
 while run:
-    pygame.time.delay(16)
-
+    pygame.time.delay(100)
     fx, fy, stop = get_forces()
 
-    if stop == False:
-        vx += fx / 10
-        vy += fy / 10
+    if stop:
+        vx, vy = 0, 0
+        rad += 1
+        ball_color = EXPLOSION
+        if rad >= 15:
+            run = False
+            rad = 0
+    else:
+        vx += fx
+        vy += fy
         x += vx
         y += vy
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        x -= 20
+        vx -= av
     if keys[pygame.K_RIGHT]:
-        x += 20
+        vx += av
     if keys[pygame.K_UP]:
-        y -= 20
+        vy -= av
     if keys[pygame.K_DOWN]:
-        y += 20
+        vy += av
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -76,9 +85,10 @@ while run:
 
     for i in all_moons:
         Moon(screen, i[0], i[1], i[2])
-    pygame.draw.rect(screen, (0, 100, 255), (x, y, 5, 5))
 
+    pygame.draw.circle(screen, ball_color, (x, y), rad)
     win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0))
-    win.blit(screen, myfont.render("Some Text", False, (0, 0, 0)), (0, 0))
+    # text = font.render("Highscore: ", 2, (255, 255, 255))
+    # win.blit(text, (10, 10))
     pygame.display.update()
 pygame.quit()
